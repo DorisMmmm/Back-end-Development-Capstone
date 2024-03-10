1024d062f74da71fb6de03683f04e2e0c4c9f1a1
DorisMmmm <doris.mueller2@de.bosch.com>
2024-03-10 08:30:12 -0400

    migrated

--- concert/models.py
+++ concert/models.py
@@ -58,8 +58,10 @@ class Song(models.Model):
     id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=255)
     lyrics = models.TextField()
-    class Meta:
-        managed = False
 
-    def __str__(self):
-        return self.title
+
+class Meta:
+    managed = False
+
+def __str__(self):
+    return self.title
